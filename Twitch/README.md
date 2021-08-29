f5adc27849a32ab0b0b3de3878c8acc127db36a5
Linux21123 <64217948+linux21123@users.noreply.github.com>
2021-08-29 03:10:54 +0200

    Add files via upload

---
+++ Twitch/README.md
@@ -0,0 +1,28 @@
+# Twitch-flood-bot
+a simple script that uses twitch OAuths and logs in the Twitch irc server to then flood a specific channel
+
+# How to use it? (Linux users)
+(YOU MUST HAVE PYTHON 2.7)
+the usage of this script is: 
+- python twfloodchannel.py (channel name) (delay) (message)
+- how to stop it? press ctrl + \
+
+want to use the whisper/privatemessage script? same usage: python twfloodwhisper.py (username) (delay) (message)
+
+# How to use it? (Windows users)
+(YOU MUST HAVE PYTHON 2.7)
+to use the script:
+- open the terminal (cmd)
+- go to the folder where the script is with "cd (folder where your script is)"
+- execute it with "twfloodchannel.py (channel) (delay) (message)
+- want to use the whisper flood script? go to the folder where the script is with "cd (folder)"
+- execute it "twfloodwhisper.py (username) (delay) (message)"
+- how to stop it? close the terminal window
+
+ # How do i add bots on the list?
+ just go to https://twitchapps.com/tmi/ then click "Connect with twitch" and add the oauth password to the tokens.txt file
+ ![alt text](https://image.prntscr.com/image/m6Dz0TM0TFay1k1BViVGSw.png)
+ 
+ # Need help?
+ discord: https://discord.gg/EFFT2Mj
+ telegram: https://t.me/tfchat
---
+++ Twitch/twfloodwhisper.py
@@ -0,0 +1,99 @@
+import socket
+import sys
+import threading
+import random
+import time
+import string
+import os
+import textwrap
+
+readbuffer = ""
+MODT = False
+
+os.system("clear")
+print textwrap.dedent("""
+..........:?77=~::::::::=+7I~,..........
+.......,?7=,,:~~~~~~~~~~=~~::+7?,.......
+....,:I?,::::~:::::~:~~~=~~=~=:~I?,.....
+....?I:::~::::::::::::~~:~~=====~~I+....
+..:7~,:::~=,+I,:::::::::::$+~+====:+7,..
+.~7,~~:O+,$$+ZO7=,,::::+$O$?OI:7$=+~~7:.
+~7:~~~:~$ZI7$:,.=O=,,+O=~~=Z77Z7=+=+=~$,
+7::~:~~::,,,,,,,,,,,,:~:~~~~=~====++?=~7
++:~~~:::::,,,,,,,,,,:::::~~~~=====+=++~?
+:~~~~::::::,,,,,,::::::::~~~~=====++++=~
+:=~~~~:~::::,:,:~:,::::~=~~~======+++++~
+~=~~~~~~~:::::OO$Z88O88Z$8I~~====++?++?~
+~==~=~~~~~::=8OZZZZ$$$ZZ$O8I====+++?++?~
+~+==~~~~~~:I8OOOOOOOOOOOOOO8?==++++++?+:
+~=+===~=~?O888OOZ$$Z8O$$OOOO8O+++++++?+:
+:=+=====~=~+88OZ$7I?I+II$$ZO7++++++++?=~
++~+=+=======$OOOZZZ$$$$$ZZO8++??++++++~?
+$~=+++===+==+8OOOOOOOOOOOO8Z+++++++?+~=7
+:$:=?++++++++78OOOOZOOOOO8O?++?+?+??=:7:
+.=7:=?+?+++++??Z88888888O7+++++++??~=Z:.
+..,7=~+?++++++?I?IIIIIIII??+?+++++:?I...
+...,?I:=++??++++++++++++?+++++++~~7+....
+.,,,,:I?~~++++++++++?++++++++=~~7?:,,.,:
+..,:,,,:?7+:~=++++++++++++=~~?7+::,.:I7I
+...,,,,,,,=?7I~::::~~:::~+I7?=:,,,.:IIII
+""")
+print "[----] Maria (is talk and destroy twitch) By Zen and Neon [----]"
+
+def randomString():
+	return ''.join([random.choice('abcdefghijklmnopqrstuvwxyz0123456789') for _ in range(0, 8)])
+
+def floodThread(socket, user, text, delay):
+	while True:
+		socket.send('PRIVMSG ' + user + ' :' + text + '\r\n')
+		time.sleep(delay)
+		socket.send('PRIVMSG ' + user + ' :' + text + '_' + randomString() + '\r\n')
+		time.sleep(delay)
+try:
+    user = sys.argv[1]
+    delay = int(sys.argv[2])
+    message = ' '.join(sys.argv[3:])
+    print "[-] '" + message + "' to twitch.tv/" + user
+    tokens = open('tokens.txt').read().split('\n')
+    for x in tokens:
+        s = socket.socket()
+        s.connect(('irc.twitch.tv', 6667))
+        s.send('PASS ' + x + '\r\n')
+        s.send('NICK ' + randomString() + '\r\n')
+        t = threading.Thread(target=floodThread, args=(s, user, message, delay,),)
+        t.start()
+except Exception as e:
+	print "[#] Maria-error: " + str(e)
+	
+while True:
+    readbuffer = readbuffer + s.recv(1024)
+    temp = string.split(readbuffer, "\n")
+    readbuffer = temp.pop()
+ 
+    for line in temp:
+        # Checks whether the message is PING because its a method of Twitch to check if you're afk
+        if (line[0] == "PING"):
+            s.send("PONG :tmi.twitch.tv\r\n")
+        else:
+            # Splits the given string so we can work with it better
+            parts = string.split(line, ":")
+ 
+            if "QUIT" not in parts[1] and "JOIN" not in parts[1] and "PART" not in parts[1]:
+                try:
+                    # Sets the message variable to the actual message sent
+                    message = parts[2][:len(parts[2]) - 1]
+                except:
+                    message = ""
+                # Sets the username variable to the actual username
+                usernamesplit = string.split(parts[1], "!")
+                username = usernamesplit[0]
+               
+                # Only works after twitch is done announcing stuff (MODT = Message of the day)
+        if MODT:
+            print username + ": " + message
+					
+					
+ 
+        for l in parts:
+            if "End of /NAMES list" in l:
+                MODT = True 
